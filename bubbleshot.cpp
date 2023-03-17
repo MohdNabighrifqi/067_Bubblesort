f7dfb85ca42a18bacf77cce882204b1dfa842794
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-17 19:19:11 +0700

    memasukan variabel a

---
+++ bubbleshot.cpp
@@ -0,0 +1,7 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int a[20];				// Deklarasi array a dengan ukuran 20

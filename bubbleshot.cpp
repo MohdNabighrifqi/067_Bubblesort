ea2e1e24dbb7fbce684400d2bd54c0d3c04bc923
Mohd Nbigh Rifqi <mohd.nabigh.ft22@mail.umy.ac.id>
2023-03-17 19:19:48 +0700

    memasukan variabel n

--- bubbleshot.cpp
+++ bubbleshot.cpp
@@ -1,7 +1,6 @@
-// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
 
 int a[20];				// Deklarasi array a dengan ukuran 20
+int n;					// Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
